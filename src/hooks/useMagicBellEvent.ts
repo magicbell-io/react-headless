93e04ca100cb508b6625703bd9364e9d2d9a28bf
Josue Montano <josuemontanoa@gmail.com>
2022-01-17 23:53:50 -0400

    chore: emit all events

--- src/hooks/useMagicBellEvent.ts
+++ src/hooks/useMagicBellEvent.ts
@@ -1,19 +1,18 @@
 import { useEffect } from 'react';
-
-import { pushEventAggregator } from '../lib/realtime';
+import { eventAggregator } from '../lib/realtime';
 
 /**
- * React hook to listen to realtime events.
+ * React hook to listen to events.
  *
  * @param event Name of the event
  * @param handler
  */
 export default function useMagicBellEvent(event: string, handler: (data?: any) => void) {
   useEffect(() => {
-    pushEventAggregator.on(event, handler);
+    eventAggregator.on(event, handler);
 
     return () => {
-      pushEventAggregator.off(event, handler);
+      eventAggregator.off(event, handler);
     };
   }, []);
 }
